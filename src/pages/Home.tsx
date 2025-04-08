70f6305b33cdf565cdec090002ba633a372a0fa0
EzequielMelo <ezequielanthonymelo@gmail.com>
2025-04-08 17:47:36 -0300

    se quito el uso del modelo 3d en dispositivos mobiles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,25 @@ import Spline from "@splinetool/react-spline";
 import { HoverBorderGradient } from "../components/ui/hover-border-gradient";
 import { projects } from "../data/projects";
 import ProjectCards from "../components/ProjectCards";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Función para detectar si el ancho es menor a 768px (mobile)
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Verifica el tamaño al cargar
+    handleResize();
+
+    // Escucha los cambios de tamaño
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const openCV = () => {
     // Abre una nueva ventana para mostrar el archivo PDF del CV
     const newWindow = window.open(
@@ -17,20 +34,28 @@ const Home = () => {
       newWindow.focus();
     }
   };
+
   return (
     <>
       <div className="relative w-full h-screen bg-black">
-        {/* Spline 3D Background */}
-        <div className="block">
+        {/* Imagen para mobile */}
+        <img
+          src="backgroundRobot.jpg" // <- poné la ruta a tu imagen aquí
+          alt="Render"
+          className="block md:hidden w-full h-full object-cover"
+        />
+
+        {/* Escena de Spline para desktop */}
+        {!isMobile && (
           <Spline
-            className="absolute inset-0 spline pointer-events-none"
+            className="absolute inset-0 pointer-events-none"
             scene="https://prod.spline.design/rgdhqiAjcPRlgu3n/scene.splinecode"
           />
-        </div>
+        )}
 
         {/* Texto encima */}
         <div className="absolute top-9/20 left-7 md:top-2/10 md:left-6/10 select-none max-[460px]:left-1/2 max-[460px]:-translate-x-1/2 max-[460px]:w-full z-20">
-          <div className="flex flex-col items-center space-y-4 md:m-0 md:p-0 bg-gray-950/40 rounded-lg shadow-lg p-4 m-2">
+          <div className="flex flex-col items-center space-y-4 md:m-0 md:p-0 bg-gray-950/40 rounded-lg md:rounded-none md:bg-transparent shadow-lg p-4 m-2">
             <h1 className="font-bold text-white text-[90px] sm:text-[120px] md:text-[170px] m-0 p-0">
               Hola!
             </h1>
@@ -89,10 +114,10 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="absolute bottom-0 md:bottom-5 md:right-2 bg-black w-full h-14 md:w-50 md:h-10"></div>
+        <div className="absolute md:bottom-5 md:right-2 md:bg-black md:w-50 md:h-10"></div>
       </div>
 
-      <div className="bg-gradient-to-b from-black via-[#111111] to-[#111111] flex flex-col items-center h-full px-4 md:px-8 lg:px-16">
+      <div className="bg-gradient-to-b from-black via-[#111111] to-[#111111] flex flex-col items-center h-full px-4 md:px-8 lg:px-16 pt-4">
         <div className="max-w-[1500px] m-auto select-none">
           <section>
             <p className="text-xl md:text-2xl lg:text-3xl font-medium text-[#636363] text-pretty md:leading-[2rem] lg:leading-[3rem]">
