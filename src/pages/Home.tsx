f84a7737e47d880e6c78188ae3ffd8e7c8bcad95
EzequielMelo <ezequielanthonymelo@gmail.com>
2025-04-09 14:35:23 -0300

    se aplico una imagen mia en formato ghibli y se arreglo el cv que estaba mal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -119,8 +119,9 @@ const Home = () => {
 
       <div className="bg-gradient-to-b from-black via-[#111111] to-[#111111] flex flex-col items-center h-full px-4 md:px-8 lg:px-16 pt-4">
         <div className="max-w-[1500px] m-auto select-none">
+          <section className="bg-[url('me.png')] bg-[70%_40%] md:bg-[25%_28%] w-full h-[60vh] lg:h-[90vh] mt-10 bg-cover rounded-3xl border-2 border-[#272727]"></section>
           <section>
-            <p className="text-xl md:text-2xl lg:text-3xl font-medium text-[#636363] text-pretty md:leading-[2rem] lg:leading-[3rem]">
+            <p className="text-xl md:text-2xl lg:text-3xl font-medium text-[#636363] text-pretty md:leading-[2rem] lg:leading-[3rem] mt-8">
               Apasionado por el mundo de la programación y la tecnología, me
               dedico a desarrollar{" "}
               <span className="text-2xl md:text-3xl lg:text-4xl font-cormorant italic  font-bold text-white">
@@ -171,7 +172,7 @@ const Home = () => {
           >
             Proyectos
           </h3>
-          <div className="grid-cols-1 md:grid-cols-2 grid gap-4 gap-y-6 mt-10">
+          <div className="grid-cols-1 md:grid-cols-2 grid gap-4 gap-y-6 mt-8">
             {projects.map((project) => (
               <ProjectCards key={project.id} {...project} />
             ))}
